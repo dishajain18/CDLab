2dbddd7b23580ac8fad9a30ec4f3596c98599938
Disha Jain <dishajain1802@gmail.com>
2025-01-20 23:03:44 +0530

    Update q1.c

--- Lab2/q1.c
+++ Lab2/q1.c
@@ -1,21 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main()
-{
-	FILE* fptr1,fptr2;
-	char filename[100];
-	char c;
-	printf("Enter the filename to open for reading: \n");
-	scanf("%s", filename);
-	fptr1 = fopen(filename, "r");
-	if (fptr1 == NULL)
-	{
-		printf("Cannot open file %s \n", filename);
-		exit(0);
-	}
-	printf("Enter the filename to open for writing: \n");
-	scanf("%s", filename);
-	fptr2 = fopen(filename, "w+");
 
-	
+int main() {
+    FILE *fptr1, *fptr2;
+    char filename[100];
+    char cur, prev = 0;
+
+    printf("Enter the input file name: ");
+    scanf("%[^\n]c", filename);
+    
+    fptr1 = fopen(filename, "r");
+    if (fptr1 == NULL) {
+        perror("Error opening input file");
+        return -1;
+    }
+
+    printf("Enter the output file name: ");
+    scanf(" %[^\n]c", filename);
+
+    fptr2 = fopen(filename, "w+");
+    if (fptr2 == NULL) {
+        perror("Error opening output file");
+        fclose(fptr1);
+        return -1;
+    }
+
+    while ((cur = fgetc(fptr1)) != EOF) {
+        if (cur == ' ' || cur == '\t') {
+            if (prev != ' ') {
+                fputc(' ', fptr2);
+            }
+        } else {
+            fputc(cur, fptr2);
+        }
+        prev = cur;
+    }
+
+    // Close the files
+    fclose(fptr1);
+    fclose(fptr2);
+
+    printf("Output written to: %s\n", filename);
+
+    return 0;
 }
