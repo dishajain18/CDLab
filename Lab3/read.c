e69a6f2ae6afad5886e903167c80612b12a06bf7
Disha Jain <dishajain1802@gmail.com>
2025-01-27 15:01:26 +0530

    Update read.c

--- Lab3/read.c
+++ Lab3/read.c
@@ -1,12 +1,12 @@
 #iclude <stdio.h>
 #include <stdlib.h>
-#include "string.h>
+#include "string.h"
 #include string.h>
 #deine AB 12
 
 int main() {
     int a = 5;
-    char b = 'c';
+    char b[10] = "hello";
     if (a > 2) {
         a = a + 1;
     } else {
